39372b79073e2322c35e249079ba0f8acf4ae60d
Marta Bylewska <martabylewska@gmail.com>
2022-10-21 13:09:11 +0200

    single blog page and metadata

--- themes/sustainable-theme/archive.php
+++ themes/sustainable-theme/archive.php
@@ -3,7 +3,7 @@
 
 <section class="page-wrap">
 
-
+    <h1><?php echo single_cat_title(); ?></h1>
 
     <?php get_template_part('includes/section', 'archive'); ?>
     <?php previous_posts_link(); ?>
---
+++ themes/sustainable-theme/includes/section-blogcontent.php
@@ -0,0 +1,38 @@
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+        <?php echo get_the_date("l d/m/y"); ?>
+
+        <p><?php
+            $categories = get_the_category();
+            foreach ($categories as $cat) : ?>
+
+                <a href="<?php echo get_category_link($cat->term_id); ?>">
+                    <?php echo $cat->name; ?>
+                </a>
+
+            <?php endforeach; ?>
+        </p>
+
+
+
+        <?php the_content(); ?>
+
+
+        <?php
+        $fname = get_the_author_meta('first_name');
+        // $lname = get_the_author_meta('last_name');
+        ?>
+        <p><i> Posted by <?php echo $fname; ?></i></p>
+
+        <p><?php
+            $tags = get_the_tags();
+            foreach ($tags as $tag) : ?>
+                <a class="tags" href="<?php echo get_tag_link($tag->term_id); ?>">
+                    <?php echo $tag->name; ?>
+                </a>
+            <?php endforeach; ?>
+        </p>
+
+
+<?php endwhile;
+else : endif; ?>
