c9068eae84f2412f4c211fb0a0007980d7a70a5f
Marta Bylewska <martabylewska@gmail.com>
2022-10-28 13:44:44 +0200

    registering widget and sidebar in function.php

--- themes/sustainable-theme/functions.php
+++ themes/sustainable-theme/functions.php
@@ -12,6 +12,7 @@ add_action('wp_enqueue_scripts', 'load_css');
 //theme options
 add_theme_support('menus');
 add_theme_support('post-thumbnails');
+add_theme_support('widgets');
 
 
 //menus
@@ -28,3 +29,28 @@ register_nav_menus(
 //custom image sizes
 add_image_size('blog-large', 800, 400, true);
 add_image_size('blog-small', 300, 200, true);
+
+
+//register sidebar
+function my_sidebars()
+{
+
+    register_sidebar(
+        array(
+            'name' => 'Page Sidebar',
+            'id' => 'page-sidebar',
+            'before_title' => '<h4 class="widget-title"',
+            'after_title' => '</h4>'
+        )
+    );
+
+    register_sidebar(
+        array(
+            'name' => 'Blog Sidebar',
+            'id' => 'blog-sidebar',
+            'before_title' => '<h4 class="widget-title"',
+            'after_title' => '</h4>'
+        )
+    );
+}
+add_action('widgets_init', 'my_sidebars');
