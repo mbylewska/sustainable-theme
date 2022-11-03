26d7e65859d5b133c2aecefb0290b5af4549d1cf
Marta Bylewska <martabylewska@gmail.com>
2022-11-03 11:04:47 +0100

    working with css

--- themes/sustainable-theme/functions.php
+++ themes/sustainable-theme/functions.php
@@ -13,6 +13,8 @@ add_action('wp_enqueue_scripts', 'load_css');
 add_theme_support('menus');
 add_theme_support('post-thumbnails');
 add_theme_support('widgets');
+add_theme_support('custom-header');
+
 
 
 //menus
