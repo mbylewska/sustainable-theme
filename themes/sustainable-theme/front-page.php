3cdbf777998fef9461316347f0eb5e9e7148fea8
Marta Bylewska <martabylewska@gmail.com>
2022-11-10 11:59:32 +0100

    working on front-page banner

--- themes/sustainable-theme/front-page.php
+++ themes/sustainable-theme/front-page.php
@@ -2,10 +2,19 @@
 
 
 <section class="page-wrap">
-    <div class="container">
 
+
+
+
+    <div class="header-title" style="background-image:linear-gradient(to bottom, rgba(249, 249, 249, 0.52), rgba(11, 50, 16, 1)),url(<?php the_post_thumbnail_url('blog-large'); ?>);">
         <h1><?php the_title(); ?></h1>
+    </div>
+    <!-- <img src="<?php the_post_thumbnail_url('blog-large'); ?>" alt="<?php the_title(); ?>" class="post-image"> -->
 
+
+
+    <!-- <h1><?php the_title(); ?></h1> -->
+    <div class="container">
         <?php get_template_part('includes/section', 'content'); ?>
 
 
